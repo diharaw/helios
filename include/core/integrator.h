3ad294eaedfb0e07cd1b4989cb1ba093b49fccc5
diharaw <shredhead94@gmail.com>
2020-11-05 19:35:43 +0000

    Editor UI prototype.

--- include/core/integrator.h
+++ include/core/integrator.h
@@ -9,14 +9,14 @@ class Integrator
 public:
     friend class Renderer;
 
-private:
+protected:
     std::weak_ptr<vk::Backend> m_backend;
 
 public:
     Integrator(vk::Backend::Ptr backend) : m_backend(backend) {}
     ~Integrator() {}
 
-private:
+protected:
     virtual void execute(vk::DescriptorSet::Ptr per_scene_ds, vk::DescriptorSet::Ptr per_frame_ds) = 0;
 };
 } // namespace lumen
--- include/integrator/path.h
+++ include/integrator/path.h
@@ -6,6 +6,11 @@ namespace lumen
 {
 class PathIntegrator : public Integrator
 {
+public:
+    PathIntegrator(vk::Backend::Ptr backend);
+    ~PathIntegrator();
 
+protected:
+    void execute(vk::DescriptorSet::Ptr per_scene_ds, vk::DescriptorSet::Ptr per_frame_ds) override;
 };
 } // namespace lumen
